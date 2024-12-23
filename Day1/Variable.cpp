1cbb8e74c69338b65a868e196831f1195bf3f6ce
pb319 <pranaybiswas319@gmail.com>
2024-12-23 07:22:23 +0530

    Variable Added

---
+++ Day1/Variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Variable Declaration
+    int Age = 25;
+    char Grade = 'A';
+    cout <<"My Age is: "<< Age <<" Having Grade: "<< Grade<< endl;
+
+    return 0;
+}
