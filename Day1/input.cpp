5e06debabadf7910cdc95a9b27b8973dd037a72a
pb319 <pranaybiswas319@gmail.com>
2024-12-25 09:11:57 +0530

    input added

---
+++ Day1/input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<< "Please Enter your Age: ";
+    cin>> age;
+    cout<< "Your Age is "<<age << endl;
+    return 0;
+}
+
