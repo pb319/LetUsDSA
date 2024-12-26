529e586da2cc56e198703d497431c244bfb2c2f7
pb319 <pranaybiswas319@gmail.com>
2024-12-26 23:19:27 +0530

    Operators Added

--- Day1/operator.cpp
+++ Day1/operator.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 int main(){
 
-    int a =5 , b= 1 ;
-    float sum = a+b;
+    int a1 =5 , b1= 1 ;
+    float sum = a1+b1;
     cout << sum << endl;
+
+    int a2 = 5, b2 = 2;
+    cout << a2/b2 << endl;
+    cout << a2/(float)b2 << endl;
+
+
     return 0;
+
 }
