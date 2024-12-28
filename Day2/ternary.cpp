c8b02ef439641164260e346fb5385ef7953a112b
pb319 <pranaybiswas319@gmail.com>
2024-12-28 16:35:05 +0530

    ternary operator addded

---
+++ Day2/ternary.cpp
@@ -0,0 +1,10 @@
+// write a code to implement conditionals with ternary 
+#include<iostream>
+using namespace std;
+
+int main(){
+    float num;
+    cout<<"Enter a Number\n";
+    cin>>num;
+    cout<<(num>0?"Positive\n":"Negetive\n");
+    return 0;
