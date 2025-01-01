219c28a56c426efeeda34d8ed2257a6b409e1a70
pb319 <pranaybiswas319@gmail.com>
2025-01-01 23:59:36 +0530

    ll

---
+++ Day1/2Sum.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file 2Sum.cpp
+ * @brief This file contains the implementation of the 2Sum problem.
+ * 
+ * The 2Sum problem involves finding two numbers in an array that add up to a specific target sum.
+ * This implementation provides a solution to the problem using an efficient algorithm.
+ * 
+ * @author Prasun
+ * @date Day1
+ */
+
+// Write a code to find Sum of two numbers
+#include <iostream>
+using namespace std;
