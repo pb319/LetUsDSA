70f83b49a07be6d9d43f5368345718c9729c69cb
pb319 <pranaybiswas319@gmail.com>
2025-03-05 23:55:16 +0530

    Triangle Pattern added

--- Day3/triangle_star.cpp
+++ Day3/triangle_star.cpp
@@ -4,13 +4,16 @@ using namespace std;
 int main()
 {   
     int line = 4;
+    int num = 1;
     for(int i=0;i<line;i++)
-    {
-        for(int j=0;j<line;j++)
+    {   
+        
+        for(int j=0;j<num;j++)
         {
             cout<<"*";
         }
         cout<<"\n";
+        num ++ ;
     }
     return 0;
 }
