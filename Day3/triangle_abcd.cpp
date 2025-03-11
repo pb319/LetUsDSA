adc4577f785538e46d239e4f1ba22c40fc9d3e63
pb319 <pranaybiswas319@gmail.com>
2025-03-11 13:02:16 +0530

    Letter Pattern Added

--- Day3/triangle_abcd.cpp
+++ Day3/triangle_abcd.cpp
@@ -3,5 +3,21 @@ using namespace std;
 
 int main()
 {
-    
+    int line = 4;
+    char letter = 'A';
+    for(int i = 0;i< line;i++)
+    {
+
+        // cout<<"Outer Workign";
+        for(int j = 0; j<= i;j++)
+        {
+            // cout<<"Inner Workign";
+            cout<<letter;
+            // cout<<"It's Working";
+        }
+        letter++;
+        cout<<'\n';
+    }
+    return 0;
+
 }
