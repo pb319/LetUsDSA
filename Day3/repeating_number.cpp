5fce6251ecd8840405e3865204274fb858d63604
pb319 <pranaybiswas319@gmail.com>
2025-03-20 16:00:20 +0530

    Pattern Added

---
+++ Day3/repeating_number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int line = 4;
+    for(int i=0;i<line;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout<<j+1;
+        }
+        cout<<"\n";
+    }
+
+}
