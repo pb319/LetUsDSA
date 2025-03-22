ea54388341b731ef4ad50949eb526faa7201c9be
pb319 <pranaybiswas319@gmail.com>
2025-03-23 02:42:34 +0530

    Problem Statement Added

---
+++ Day3/holdi.cpp
@@ -0,0 +1,17 @@
+//Write a Programme to print the following Hollow Diagram
+//      * 
+//    *   *
+//  *       *
+//    *   *
+//      * 
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
