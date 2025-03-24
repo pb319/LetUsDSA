10df17ae796b65c21c602a0d764a646ed9f88e0c
pb319 <pranaybiswas319@gmail.com>
2025-03-24 08:28:11 +0530

    Top Portion Done

--- Day3/holdi.cpp
+++ Day3/holdi.cpp
@@ -12,6 +12,26 @@ using namespace std;
 
 int main()
 {
+    int n=4;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=n-i-1;j>0;j--)
+        {
+            cout<<" ";
+        }
+        cout<<"*";
+        for(int j=2*i -1; j>0;j--)
+        {
+            cout<<" ";
+        }
+        if(i!=0)
+        {
+            cout<<"*";
+        }
+        
+        cout<<"\n";
+
+    }
     
     return 0;
 }
