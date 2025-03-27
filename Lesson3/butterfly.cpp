269234d4677eafe1e3c6229d5f35865e608c632b
pb319 <pranaybiswas319@gmail.com>
2025-03-27 09:56:47 +0530

    Problem Added

---
+++ Lesson3/butterfly.cpp
@@ -0,0 +1,20 @@
+// Write a Programme to print the following
+// n = 4
+// *      *
+// **    **
+// ***  ***    
+// ********
+// ***  ***
+// **    **
+// *      *
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
