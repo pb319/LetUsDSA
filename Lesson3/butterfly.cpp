49e3d4ccaedb63463f7cb0e1b1ea2a73a01bca03
pb319 <pranaybiswas319@gmail.com>
2025-03-30 14:03:23 +0530

    Butterfly Added

--- Lesson3/butterfly.cpp
+++ Lesson3/butterfly.cpp
@@ -15,6 +15,62 @@ using namespace std;
 
 int main()
 {
+    int line = 4;
+    for(int i=0; i<line;i++)
+    {
+        for(int j=0;j<i+1;j++)
+        {
+            cout<<"*";
+        }
+
+        for(int j=line-i-1;j>0;j--)
+        {
+            cout<<" ";
+        }
+
+        for(int j=line-i-1;j>0;j--)
+        {
+            cout<<" ";
+        }
+
+        for(int j=0;j<i+1;j++)
+        {
+            cout<<"*";
+        }
+
+        
+        cout<<endl;
+    }
+
+
+    for(int i=0; i<line-1;i++)
+    {
+        
+
+        for(int j=line-i-1;j>0;j--)
+        {
+            cout<<"*";
+        }
+        for(int j=0;j<i+1;j++)
+        {
+            cout<<" ";
+        }
+
+        for(int j=0;j<i+1;j++)
+        {
+            cout<<" ";
+        }
+
+        for(int j=line-i-1;j>0;j--)
+        {
+            cout<<"*";
+        }
+        
+        cout<<endl;
+    }
+
+
+
     
     return 0;
 }
