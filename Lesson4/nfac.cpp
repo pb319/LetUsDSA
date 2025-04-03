4aeb74aafe6642653f34e895633e449d0cfb104c
pb319 <pranaybiswas319@gmail.com>
2025-04-03 08:43:47 +0530

    N Factorial Function added

---
+++ Lesson4/nfac.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int nFac(int N)
+{
+    int mul = 1;
+    for(int i=1;i<=N;i++)
+    {
+        mul *= i;
+    }
+    return mul;
+
+}
+
+
+int main()
+{
+    cout<<"Result :"<<nFac(5)<<endl;
+    return 0;
+}
