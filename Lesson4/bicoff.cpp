18c337235cbdc3dd137a59394ada1daf5f9cd623
pb319 <pranaybiswas319@gmail.com>
2025-04-06 10:20:43 +0530

    Binomial Coefficient Added

---
+++ Lesson4/bicoff.cpp
@@ -0,0 +1,31 @@
+// Write a Programme to calculate the binomial coefficient(nCr) for a given n and r
+
+#include<iostream>
+using namespace std;
+
+int fac(int num)
+{   
+    float mul = 1;
+    for(int i=0;i<num;i++)
+    {
+        mul *= i+1;
+    }
+    return mul;
+}
+
+int binCoff(int n, int r)
+{
+    float coff = 1;
+    for(int i = 0; i<r; i++)
+    {
+        coff *= n;
+        n--;
+    }
+    return (float)coff/fac(r);
+}
+
+int main()
+{
+    cout<<binCoff(5,3)<<endl;
+    return 0;
+}
