cd37d6a08f39e2dc7e3da6fc7fe7e3df59981b69
pb319 <pranaybiswas319@gmail.com>
2025-04-07 08:33:44 +0530

    Prime Checker Added

--- Lesson4/prime.cpp
+++ Lesson4/prime.cpp
@@ -2,17 +2,30 @@
 #include<iostream>
 using namespace std;
 
-int checkPrime(int num)
+void checkPrime(int num)
 {
-    for(int i=0;i<num-1;i++)
+    for(int i=1;i<num-1;i++)
     {
 
+
+        if (num % (i+1) == 0)
+        {
+            cout<<"Not a Prime\n";
+            return;
+        }
+        
     }
+
+    cout<<"Prime\n";
+    return;
+    
 }
 
 
 int main()
 {
-
-
+    checkPrime(2);
+    checkPrime(22);
+    checkPrime(32);
+    return 0;
 }
