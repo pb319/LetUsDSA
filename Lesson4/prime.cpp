c9fdd868e547d1d987991d0ac13cef5016977495
pb319 <pranaybiswas319@gmail.com>
2025-04-07 08:03:21 +0530

    Prime Checker in Progress

---
+++ Lesson4/prime.cpp
@@ -0,0 +1,18 @@
+//Write a Function to check if a number is Prime
+#include<iostream>
+using namespace std;
+
+int checkPrime(int num)
+{
+    for(int i=0;i<num-1;i++)
+    {
+
+    }
+}
+
+
+int main()
+{
+
+
+}
