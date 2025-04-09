8860e15366077bab78ce5dc9a3a7e9a5eb4ba535
pb319 <pranaybiswas319@gmail.com>
2025-04-09 22:09:53 +0530

    Call by Reference

--- Lesson7/pass2ref.cpp
+++ Lesson7/pass2ref.cpp
@@ -9,10 +9,41 @@ void changeVar(int x)
     return;
 }
 
+void changeArr(int arr[],int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] += 2;
+    } 
+    
+}
+
 int main()
 {   
     int x = 5;
-    changeVar(x); // Pass by Value
+    int arr[]={1,2,3};
+
+    // Pass by Value
+    changeVar(x); 
     cout<<x<<endl; // no change in x
+    
+    // Pass by Reference
+    cout<<"Before function call: "<<"\n";
+    for (int i = 0; i < 3; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+
+    changeArr(arr,3); // array changed in main() scope as well
+    cout<<"After function call: "<<"\n";
+    for (int i = 0; i < 3; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    
+    
     return 0;
 }
