06f5c301c7354f666815f30fc08ab83cdcc829c0
pb319 <pranaybiswas319@gmail.com>
2025-04-11 20:17:50 +0530

    Vector Methods Added

---
+++ Lesson8/ec_mehods.cpp
@@ -0,0 +1,10 @@
+// Write a programme to depict usage of various mehods wihin vectors
+//size, push_back, pop_back, front, back, at
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+}
+
--- Lesson8/init.cpp
+++ Lesson8/init.cpp
@@ -10,9 +10,18 @@ int main()
     vector <int> vec2 = {1,2,3};
     vector <int> vec3(3,7); //vector with 3 elements all being 7
 
+    cout<<"Next Vector: "<<endl;
     for(int i=0;i<3;i++)
     {
         cout<<vec2[i]<<endl;
     }
+
+    cout<<"Next Vector: "<<endl;
+    // for each loop
+    for(int i: vec3)  // synax
+    {
+        cout<<i<<endl;
+    }
+
     return 0;
 }
