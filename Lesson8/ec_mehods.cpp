9eb34ad21f153836a69926efb2c9388bddf3090f
pb319 <pranaybiswas319@gmail.com>
2025-04-12 09:28:17 +0530

    Methods Added

--- Lesson8/ec_mehods.cpp
+++ Lesson8/ec_mehods.cpp
@@ -16,13 +16,31 @@ cout<<vec.size()<<endl;
 int count;
 cout<<"How many numbers you want to insert??: "<<endl;
 cin>>count;
-cout<<"\n";
 
+cout<<"Enter your integers: "<<endl;
 for(int i=0;i<count;i++)
 {
-
+    int num;
+    cin>>num;
+    vec.push_back(num);
 }
 
+// vec.push_back(1);
+// vec.push_back(3);
+// vec.push_back(5);
 
+cout<<"Let's see what you have: "<<endl;
+for(int val: vec)
+{
+    cout<<val<<"\n";
+    
+}
+cout<<"Size: "<<vec.size()<<endl;
+
+//Accessign Element at a specific index 
+ cout<< "Element at 2nd Place: "<<vec.at(1)<< " ";
+
+
+return 0;
 }
 
