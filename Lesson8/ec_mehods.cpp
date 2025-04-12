798954748a359dabc1de77ab4947161731c863ab
pb319 <pranaybiswas319@gmail.com>
2025-04-12 09:02:27 +0530

    Vectorial Methods Added

--- Lesson8/ec_mehods.cpp
+++ Lesson8/ec_mehods.cpp
@@ -1,10 +1,28 @@
 // Write a programme to depict usage of various mehods wihin vectors
 //size, push_back, pop_back, front, back, at
 #include<iostream>
+#include<vector>
 using namespace std;
+
 
 int main()
 {
+vector <int> vec;
+
+//size of the vector
+cout<<vec.size()<<endl;
+
+//inserting elements from end
+int count;
+cout<<"How many numbers you want to insert??: "<<endl;
+cin>>count;
+cout<<"\n";
+
+for(int i=0;i<count;i++)
+{
+
+}
+
 
 }
 
