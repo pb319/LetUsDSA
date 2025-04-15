adcd2d0f4da665941c9ec0405a601c10a62c414f
pb319 <pranaybiswas319@gmail.com>
2025-04-15 23:11:51 +0530

    Lesson9 Updated

--- Lesson9/MaxSub.cpp
+++ Lesson9/MaxSub.cpp
@@ -1,1 +1,24 @@
 // Write a Programme to print continuous subarrays
+#include<iostream>
+using namespace std;
+
+void subArray(int arr[], int size){
+    for(int st=0;st<size;st++){
+        for(int en=st;en<size;en++){
+            for(int i = st;i<=en;i++){
+                cout<< arr[i];
+            }
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
+    return;
+}
+
+
+int main(){
+
+    int arr[]= {1,2,3,4,5};
+    subArray(arr,5);
+    return 0;
+}
