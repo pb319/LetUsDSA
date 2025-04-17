3f4367c9ec2e94ecb02e659765be0d9b1f905318
pb319 <pranaybiswas319@gmail.com>
2025-04-17 11:59:55 +0530

    majorityBrute.cpp

---
+++ Lesson10/majorityBrute.cpp
@@ -0,0 +1,15 @@
+//Write a programme to find majority element (Brute Force Approach)
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector <int> majorityBrute(vector<int> nums){
+
+}
+
+
+
+int main(){
+    cout<< 7/2;
+    return 0;
+}
