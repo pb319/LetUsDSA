ef6ba99793f7cea259d2e0e25a1cfa4afa60e16b
pb319 <pranaybiswas319@gmail.com>
2025-04-24 11:14:24 +0530

    Bubble Sort Added

--- Lesson11/bubble.cpp
+++ Lesson11/bubble.cpp
@@ -1,1 +1,25 @@
 //Write a programme to sort a given array using "Bubble Sort" 
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void bubbleSort(vector <int>& nums){
+    int n =(nums.size());
+    for(int i=0; i< n-1; i++){
+        for(int j=0;j<n-i-1;j++){
+            if(nums[j]>nums[j+1]){
+                swap(nums[j],nums[j+1]);
+            }
+        }
+    }
+}
+
+
+int main(){
+    vector <int> nums = {5,2,3,1};
+    bubbleSort(nums);
+    for(int val:nums){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+}
