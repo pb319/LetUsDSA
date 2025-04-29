188f508815e0e5e987865471acc0cc08dbd023da
pb319 <pranaybiswas319@gmail.com>
2025-04-29 08:02:59 +0530

    fibonacci in recursion added

---
+++ Lesson11/fibonacci.cpp
@@ -0,0 +1,23 @@
+// Write a Programme to print Fibonacci Series using recursion
+#include<iostream>
+using namespace std;
+
+int fibboRec(int n){
+    if (n == 0){
+        return 0;
+    }
+    else if (n==1){
+        return 1;
+    }else{
+       return fibboRec(n-1) + fibboRec (n-2) ;
+    }
+    
+}
+
+int main(){
+    for(int i=0; i<=10; i ++ ){
+        cout<< fibboRec(i)<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
