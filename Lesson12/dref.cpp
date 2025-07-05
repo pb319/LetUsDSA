90b21d068f7d9785acd89f5784767cc3249e8cac
pb319 <pranaybiswas319@gmail.com>
2025-07-05 16:48:04 +0530

    Dereferencing Operator

---
+++ Lesson12/dref.cpp
@@ -0,0 +1,12 @@
+//Write a programme to demonstrate usage of dereferencing operator
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    int* ptr = &a;
+
+    cout<<"Value at a: "<< a <<endl;
+    cout<<"Value at adress o the pointer: "<< *(ptr)<< endl;
+    return 0;
+}
