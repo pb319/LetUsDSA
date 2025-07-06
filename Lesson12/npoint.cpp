143e41d6cd0e03d1a320fc053d135d963eb79d53
pb319 <pranaybiswas319@gmail.com>
2025-07-06 14:29:05 +0530

    Null Pointer Demonstarted

---
+++ Lesson12/npoint.cpp
@@ -0,0 +1,13 @@
+//Write a programme to demonstrate usage of a null pointer
+#include<iostream>
+using namespace std;
+
+int main(){
+    int* ptr1;
+    int* nptr = NULL;
+
+    cout<<ptr1<<endl;
+    cout<<nptr<<endl;
+
+    return 0;
+}
