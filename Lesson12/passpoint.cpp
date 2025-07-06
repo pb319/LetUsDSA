16e152aab0fbd13e3529c580930c0a37d6a39a84
pb319 <pranaybiswas319@gmail.com>
2025-07-06 17:34:09 +0530

    Pass by Reference Updated

--- Lesson12/passpoint.cpp
+++ Lesson12/passpoint.cpp
@@ -2,22 +2,31 @@
 #include<iostream>
 using namespace std;
 
-int change_pbv(int a){
+int change_pbv(int a){    // Pass by Value
     a = 20;
     cout<< "outside main: "<< a<<endl;
     return a;
 }
 
-void change_pbr(int* ptr){
-    *(ptr) = 20;
+void change_pbp(int* ptr){ // Pass by Ref(Pointer)
+    *(ptr) += 20;
 }
+
+void change_pba(int &b){ // Pass by Ref(Alias)
+    b += 20;
+}
+
 
 
 int main(){
     int a = 10;
-    change_pbv(a);
+    change_pbv(a);      
     cout<< "pass by value inside main: "<< a<<endl;
-    change_pbr(&a);
-    cout<< "pass by ref inside main: "<< a<<endl;
+    change_pbp(&a);     
+    cout<< "pass by pointer inside main: "<< a<<endl;
+    change_pba(a);     
+    cout<< "pass by alias inside main: "<< a<<endl;
+    
+
     return 0;
 }
