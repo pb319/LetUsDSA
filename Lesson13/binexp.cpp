1fa3abca5af56cee26e07b0ff368e7235854c25b
pb319 <pranaybiswas319@gmail.com>
2025-07-10 11:36:48 +0530

    Binary exponentiation added

--- Lesson13/binexp.cpp
+++ Lesson13/binexp.cpp
@@ -13,6 +13,30 @@ long int simpleMul(long int x,long int n){
     return mul;
 }
 
+long int binexMul(long int x,long int n){
+    long int ans = 1;
+    if(n<0){
+        x = 1/x;
+        n *= -1;
+    }
+    
+    while (n>0)
+    {
+        if (n%2==1)
+        {
+            ans *= x;
+            x *= x;   
+        }else{
+            x *= x;
+        }
+        n/=2;
+        
+    }
+    
+    return ans;
+}
+
+
 int main(){
     long int x,n;
     cout<<"Enter X: "<<endl;
@@ -20,5 +44,6 @@ int main(){
     cout<<"Enter N: "<<endl;
     cin>>n;
     cout<<"pow(X,N): "<<simpleMul(x,n)<<endl;
+    cout<<"pow(X,N) - Binary Exponentiation: "<<binexMul(x,n)<<endl;
     return 0;
 }
