c66f2a597e8f6085822c24ae2b0a6644625e566d
pb319 <pranaybiswas319@gmail.com>
2025-08-04 06:32:53 +0530

    Optimized Stock Buy&Sell Added

--- Lesson13/stock-optimized.cpp
+++ Lesson13/stock-optimized.cpp
@@ -1,8 +1,30 @@
 // Write a program to buy and sell stock with maximum profit (Optimized)
 #include<iostream>
+#include<vector>
 using namespace std;
 
+int bestBuy(vector<int> prices){
+    int maxProfit =0;
+    int bestBuy = prices[0];
+    for(int i=0;i<prices.size();i++){
+        if(prices[i]>bestBuy){
+            maxProfit=max(maxProfit,prices[i]-bestBuy);
+        }
+        bestBuy=min(bestBuy,prices[i]);
+    }
+
+    return maxProfit;
+}
+
 int main(){
-    
+    vector <int> arr = {7,1,5,3,6,4};
+    for(int val : arr){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    cout<<"Array Size: "<<arr.size();
+    cout<<endl;
+    cout<<"Maximum Profit: "<< bestBuy(arr);
+    cout<<endl;
     return 0;
 }
