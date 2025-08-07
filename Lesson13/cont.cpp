30e3c16bc21dec192825e407b8eda08e1cdd6824
pb319 <pranaybiswas319@gmail.com>
2025-08-07 11:31:22 +0000

    Bruteforce Container Added

--- Lesson13/cont.cpp
+++ Lesson13/cont.cpp
@@ -1,7 +1,29 @@
 // Write a program to return the maximum amount of water container can store
+// container = {1,8,6,2,5,4,8,3,7}
 #include<iostream>
+#include<vector>
+#include<limits.h>
 using namespace std;
 
+int bruteCont(vector <int> cont){
+    int h1,h2;
+    int height,weidth;
+    int area = INT_MIN;
+    for (int i=0;i<cont.size();i++){
+        h1 = cont[i];
+        for(int j=i+1;j<cont.size();j++){
+            h2 = cont[j];
+            weidth = (j-i);
+            height = min(h1,h2);
+            area = max(area,(height*weidth));
+        }
+        
+    }
+    return area;
+}
+
 int main(){
+    vector <int> cont ={1,8,6,2,5,4,8,3,7};
+    cout<<"Container with Most Water: "<<bruteCont(cont)<<endl;
     return 0;
 }
