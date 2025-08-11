67938ace3a6bafe4fd1824771128d30556fada5e
pb319 <pranaybiswas319@gmail.com>
2025-08-11 13:49:11 +0000

    Product with Division Added

---
+++ Lesson13/divProduct.cpp
@@ -0,0 +1,31 @@
+// Given an integer array nums, return an array answer such that answer[i] is equal
+// to the product of all the elements of nums except nums[i].
+
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+
+//Using Division
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector <int> divProduct(vector <int> nums){
+    vector <int> ans;
+    int count=0;
+    int n = nums.size();
+    while(count<n){
+        
+
+    }
+    return ans;
+}
+
+int main(){
+    vector <int> nums = {1,2,3,4};
+    for(int num:nums){
+        cout<<num<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
