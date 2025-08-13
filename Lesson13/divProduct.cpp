18e7f1e253b52dfbade0812145386f7e6e2e1cf6
pb319 <pranaybiswas319@gmail.com>
2025-08-13 17:33:22 +0000

    Added Check

--- Lesson13/divProduct.cpp
+++ Lesson13/divProduct.cpp
@@ -26,7 +26,7 @@ vector <int> divProduct(vector <int> nums){
 }
 
 int main(){
-    vector <int> nums = {1,2,3,4};
+    vector <int> nums = {1,2,3,5};
     for(int num:nums){
         cout<<num<<" ";
     }
