482d4b5d1b13ea6499ed9cbe45c1f855f05493ec
pb319 <pranaybiswas319@gmail.com>
2025-08-15 11:59:25 +0530

    Product of An Array Optimized Added

--- Lesson13/optProduct.cpp
+++ Lesson13/optProduct.cpp
@@ -9,19 +9,47 @@
 using namespace std;
 
 vector <int> optProduct(vector <int> nums){
-    vector <int> prefix;
-    int count =0 ;
-    int n = prefix.size() ;
-    int fp = 0 ;
-    int bp = n-1 ;
+    int count =1 ;
+    int n = nums.size() ;
+    int fp = 1 ;
+    int bp = n-2 ;
     int pt ;
+    vector <int> prefix = {1};
+    vector <int> suffix = {1};
+    vector <int> ans;
     while(count<n){
-        
+        prefix.push_back(nums[fp-1]*prefix[fp-1]);
+        suffix.push_back(nums[bp+1]*suffix[fp-1]);
+        fp ++;
+        bp --;
         count++;
     }
+    for (int i=0; i<n;i++){
+        ans.push_back(prefix[i]*suffix[n-i]);
+    }
+    cout<<endl;
+    for (int val:prefix){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+
+    for (int val:suffix){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    return ans;
+
 }
 
 int main(){
-
+    vector <int> nums = {1,2,3,6};
+    for(int num:nums){
+        cout<<num<<" ";
+    }
+    cout<<endl;
+    for (int val: optProduct(nums)){
+        cout<<val<<" ";
+    }
+    cout<<endl;
     return 0;
 }
