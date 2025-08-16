e05e051fff3e52ad12157327e4c4be94041a819b
pb319 <pranaybiswas319@gmail.com>
2025-08-16 16:55:37 +0530

    Most Optimized Initialized

---
+++ Lesson13/moptProd.cpp
@@ -0,0 +1,33 @@
+// The most optimized (time & Space) code for Product of An Array
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector <int> productExcSel(vector <int> nums){
+    int n = nums.size();
+    vector <int> prefix(n,1);
+    vector <int> suffix(n,1);
+    vector <int> ans(n,1);
+
+    //prefix
+    for(int i=1;i<n;i++){
+        prefix[i]= prefix[i-1]*nums[i-1];
+    }
+    //suffix
+    for(int i=n-2;i>=0;i--){
+        suffix[i]=suffix[i+1]*nums[i+1];
+    }
+    // ans array
+    for(int i=1;i<n;i++){
+        ans[i]= prefix[i]*suffix[i];
+    }
+
+    return ans; 
+    
+}
+
+
+int main(){
+
+    return 0;
+}
