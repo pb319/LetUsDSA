3f5c17add0991f3ad15867303374532ceca0d90a
pb319 <pranaybiswas319@gmail.com>
2025-08-26 03:35:10 +0000

    Modified Binary Search Added

---
+++ Lesson14/modBanary.cpp
@@ -0,0 +1,15 @@
+// write a code to modify the existing program of binary search
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int modBinarySearch(vector <int> nums, int tar){
+    int n = nums.size();
+
+}
+
+
+int main(){
+
+    return 0;
+}
