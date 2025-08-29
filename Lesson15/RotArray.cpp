85539aeccc2ee247eb746cfc03ac9ab477145d89
pb319 <pranaybiswas319@gmail.com>
2025-08-29 13:31:10 +0000

    Problem Initialized

---
+++ Lesson15/RotArray.cpp
@@ -0,0 +1,13 @@
+// Write a program to find a target from a rotating-ordered array.
+// Array = [9,10,11,17,1,3,6] , target = 1
+
+#include<iostream>
+#include<vector>
+
+int findTarget(){
+    return -1;
+}
+
+int main(){
+    return 0;
+}
