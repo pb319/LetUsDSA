093e577423da52ef6f990f4debaa0ed0aff9a822
pb319 <pranaybiswas319@gmail.com>
2025-09-12 05:33:39 +0530

    Bruteforce Approach Addee

--- Lesson17/single.cpp
+++ Lesson17/single.cpp
@@ -1,1 +1,37 @@
 // Write a programme to search single element in a repeating sorte array
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int bruteSingle(vector <int> arr){
+    int n = arr.size();
+    for(int i=0;i<n;i++){
+        if(i==0){
+            if(arr[i]<arr[i+1]){
+                return i;
+            }
+        }
+        else if(i==n-1){
+            if(arr[i-1]<arr[i]);
+        }
+        else{
+            if(arr[i-1]<arr[i] & arr[i]<arr[i+1]){
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+
+
+int main(){
+    vector <int> arr = {1,1,2,2,3,4,4};
+    cout<<"Given Array: "<<endl;
+    for(int val:arr){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    cout<<"Bruteforce Single Element: "<<bruteSingle(arr)<<endl;
+    return 0;
+}
