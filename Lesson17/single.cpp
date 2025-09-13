8eb01a5446fe7d3e8004016744ce4e0681c0e7f8
pb319 <pranaybiswas319@gmail.com>
2025-09-13 14:58:31 +0530

    Optimized Single Element Updated

--- Lesson17/single.cpp
+++ Lesson17/single.cpp
@@ -1,4 +1,6 @@
 // Write a programme to search single element in a repeating sorte array
+// arr1 = [1,1,2,2,3,4,4]
+// arr1 = [1,1,2,2,3,3,4,5,5]
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -24,17 +26,66 @@ int bruteSingle(vector <int> arr){
 }
 
 int twopSingle(vector <int> arr){
+    int st = 0, en = arr.size();
+    int mid;
+    // int count=1;
+    while(st<=en){
+        mid = st + (en-st)/2;
+        // cout<<"Step: "<<count<<endl;
+
+        if(mid%2==0){
+            // cout<<"Inside 1"<<endl;
+            if(arr[mid-1]==arr[mid]){
+                // cout<<"Inside 1.1"<<endl;
+                en = mid-1;
+            }
+            else{
+                // cout<<"Inside 1.2"<<endl;
+                st = mid+1;
+            }
+        }else{
+            // cout<<"Inside 2"<<endl;
+            if(arr[mid-1]==arr[mid]){
+                // cout<<"Inside 2.1"<<endl;
+                st = mid+1;
+            }
+            else{
+                // cout<<"Inside 2.2"<<endl;
+                en = mid-1;
+            }
+
+        }
+        // cout<<"Start: "<<st<<endl;
+        // cout<<"End: "<<en<<endl;
+
+        if(st==en){
+            return st;
+        }
+        // count++;
+
+    }
     return -1;
 }
 
 
 int main(){
-    vector <int> arr = {1,1,2,2,3,4,4};
-    cout<<"Given Array: "<<endl;
-    for(int val:arr){
+    vector <int> arr1 = {1,1,2,2,3,4,4};
+    cout<<"Given Array1: "<<endl;
+    for(int val:arr1){
         cout<<val<<" ";
     }
     cout<<endl;
-    cout<<"Bruteforce Single Element: "<<bruteSingle(arr)<<endl;
+    cout<<"Bruteforce Single Element: "<<bruteSingle(arr1)<<endl;
+    cout<<"Bruteforce Single Element: "<<twopSingle(arr1)<<endl;
+    cout<<endl;
+
+    vector <int> arr2 = {1,1,2,2,3,3,4,5,5};
+    cout<<"Given Array2: "<<endl;
+    for(int val:arr2){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    cout<<"Bruteforce Single Element: "<<bruteSingle(arr2)<<endl;
+    cout<<"Bruteforce Single Element: "<<twopSingle(arr2)<<endl;
     return 0;
 }
