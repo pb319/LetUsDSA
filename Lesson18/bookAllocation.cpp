735179303f42d52cb82d205a0cab34f440807c34
pb319 <pranaybiswas319@gmail.com>
2025-09-29 04:40:55 +0530

    Problem Trimmed

--- Lesson18/bookAllocation.cpp
+++ Lesson18/bookAllocation.cpp
@@ -2,12 +2,6 @@
 // You have to allocate books to M number of students so that the maximum number of pages 
 // allocated to student is minimum.
 
-// - Eachbook should be allocated to a student
-// - Each student has to be allocated at least one book
-// - Allotment should be in contaguous manner
-
-// Calculate and return that minimum possible number. 
-// Return -1 if a valid assignment is not possible
 
 #include <iostream>
 #include <vector>
