2397cf6c6de5b75f40a7892d09ec173bb5bd2905
pb319 <pranaybiswas319@gmail.com>
2025-10-04 04:47:26 +0530

    Painter's Prlem Added

--- Lesson18/painter.cpp
+++ Lesson18/painter.cpp
@@ -4,16 +4,56 @@
 #include<vector>
 using namespace std;
 
-bool isValid(){
+bool isValid(vector <int> arr,int n, int m, int maxLim){
+    int paint=0, count = 1;
+    for (int i = 0; i < count; i++)
+    {
+        if(arr[i]>maxLim){
+            return false;
+        }
+    }
+    
+    for(int i =0;i<n;i++){
+        if(paint+arr[i]<maxLim){
+            paint += arr[i];
+        }else{
+            count++;
+            paint=arr[i];
+        }
+    }
+    if(paint==m){
+        return true;
+    }else false;
 
     return false;
 }
 
-int painterPartition(){
+int painterPartition(vector <int> arr,int n, int m){
+    int sum =0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    int st = 1, en = sum;
+    int mid, ans =-1;
+    while(st<=en){
+        mid = st + (st-en)/2;
+        if(isValid(arr,n,m,mid)){
+            ans = mid;
+            en = mid-1;
+        }else st=mid+1;
+    }
+    return ans;
+
     return -1;
 }
 
 
 int main(){
+    vector <int> arr = {5,7,8,10};
+    cout<<"Given Array: "<<endl;
+    for(int val:arr){
+        cout<<val<<" ";
+    }
+    cout<<endl<<"Painer's Partition: "<<endl;
     return 0;
 }
