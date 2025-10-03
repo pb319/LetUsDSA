f021b65368ed3ebac1e444c539c0bcaf08b49887
pb319 <pranaybiswas319@gmail.com>
2025-10-03 20:29:41 +0530

    Painter's Partition Problem Initialized

---
+++ Lesson18/painter.cpp
@@ -0,0 +1,19 @@
+// Write  a programme to find minimum time required to paint (coniguously) N boards[Length given in Array] by M Painters. Note Unit Area require Unit Time to be painted.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool isValid(){
+
+    return false;
+}
+
+int painterPartition(){
+    return -1;
+}
+
+
+int main(){
+    return 0;
+}
