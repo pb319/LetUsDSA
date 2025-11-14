8069742d90967380b16b153620b6fd58aaa00fdb
pb319 <pranaybiswas319@gmail.com>
2025-11-14 07:07:13 +0530

    Debugger Added

--- Lesson19/sort.cpp
+++ Lesson19/sort.cpp
@@ -10,10 +10,16 @@ void bubbleSorted(vector <int> &arr, int size){
     int iter = size;
 
     for(int i=0;i<size;i++){
+       cout<<"Iteration"<<i+i<<endl;
+
        for (int j=0;j<size-i;j++){
          if(arr[j]>arr[j+1]){
             swap(arr[i],arr[i+1]);
         }
+        for(int val:arr){
+        cout<<val<<" ";
+            }
+        cout<<endl;
        }
 
     }
@@ -21,6 +27,12 @@ void bubbleSorted(vector <int> &arr, int size){
 
 
 int main(){
+    vector <int> arr = {4,1,5,2,3};
+    bubbleSorted(arr,arr.size());
+    for(int val:arr){
+        cout<<val<<" ";
+    }
+    cout<<endl;
     return 0;
 }
 
