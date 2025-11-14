9b8390d73eb0844168e4583c2f43f7ccf79c9dc1
pb319 <pranaybiswas319@gmail.com>
2025-11-14 06:54:39 +0530

    Nested Loop Added

--- Lesson19/sort.cpp
+++ Lesson19/sort.cpp
@@ -8,10 +8,13 @@ using namespace std;
 //Bubble Sort --> Pushing Larger element at the end through Adjacent Comparison
 void bubbleSorted(vector <int> &arr, int size){
     int iter = size;
+
     for(int i=0;i<size;i++){
-        if(arr[i]>arr[i+1]){
+       for (int j=0;j<size-i;j++){
+         if(arr[j]>arr[j+1]){
             swap(arr[i],arr[i+1]);
         }
+       }
 
     }
 }
