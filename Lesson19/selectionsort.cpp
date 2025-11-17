31a718e9ac6dc65ff5779daff1ad103dac211d2b
pb319 <pranaybiswas319@gmail.com>
2025-11-17 21:22:58 +0530

    Selection Sort Function

--- Lesson19/selectionsort.cpp
+++ Lesson19/selectionsort.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 void selectionSort(int arr[], int size){
     for( int i=0;i<size;i++){
+        int smallIdx=i;
         for(int j=i+1;j<size;j++){
-            
+            if(arr[j]<arr[smallIdx]){
+                smallIdx = j;
+            }
         }
+        swap(arr[i],arr[smallIdx]);
     }
 }
 
