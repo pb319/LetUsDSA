6bc8b89f136c479e5518349d8cc9aebe22848205
pb319 <pranaybiswas319@gmail.com>
2025-11-17 21:10:00 +0530

    Selection Sort initialized

---
+++ Lesson19/selectionsort.cpp
@@ -0,0 +1,18 @@
+//Write a programme to sort a given array using selection sort algorithm
+#include<iostream>
+using namespace std;
+
+void selectionSort(int arr[], int size){
+    for( int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            
+        }
+    }
+}
+
+
+
+int main(){
+
+    return 0;
+}
