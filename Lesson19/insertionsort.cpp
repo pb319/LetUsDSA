e348ab80fbe343f0e187ec8d7e77c4f97e66b6cd
pb319 <pranaybiswas319@gmail.com>
2025-11-21 05:25:51 +0530

    Debugger Added

--- Lesson19/insertionsort.cpp
+++ Lesson19/insertionsort.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 void insertionSort(int arr[], int size){
-    for(int i=0;i<size;i++){
+    for(int i=1;i<size;i++){
         int curr = arr[i];
         for(int j= i-1;j>=0;j++){
             if(arr[j]>curr){
@@ -13,6 +13,11 @@ void insertionSort(int arr[], int size){
                 break;
             }
         }
+        cout<<"Status After "<< i<<endl;
+        for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
     }
 }
 
