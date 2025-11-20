624d2d4105d09dd76eff008a27bea46bb64b2c7b
pb319 <pranaybiswas319@gmail.com>
2025-11-21 05:29:24 +0530

    Logic Altered

--- Lesson19/insertionsort.cpp
+++ Lesson19/insertionsort.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void insertionSort(int arr[], int size){
     for(int i=1;i<size;i++){
         int curr = arr[i];
-        for(int j= i-1;j>=0;j++){
+        for(int j= i-1;j>=0;j--){
             if(arr[j]>curr){
                 arr[i]=arr[j];
                 arr[j]=curr;
