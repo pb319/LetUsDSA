8dbb06aef992cd8f84c66a5e42e7a7f9424bf183
pb319 <pranaybiswas319@gmail.com>
2025-11-20 22:09:16 +0530

    Logic Updated

--- Lesson19/selection.cpp
+++ Lesson19/selection.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 void selectionSort(int arr[], int size){
-    for (int i=0; i<size;i++)
+    for(int i=0;i<size;i++){
+        int curr = arr[i];
+        for(int j= i-1;j>=0;j++){
+            if(arr[j]>curr){
+                arr[]
+            }
+        }
+    }
 }
 
 int main(){
