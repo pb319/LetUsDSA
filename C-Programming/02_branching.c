1d58ff2a600106226ed1fce8413360825ce2836f
pb319 <pranaybiswas319@gmail.com>
2026-08-23 19:51:16 +0530

    Branching updated

--- C-Programming/02_branching.c
+++ C-Programming/02_branching.c
@@ -3,12 +3,26 @@ void main(){
     int val;
     printf("Enter an Integer:\n");
     scanf("%d",&val);
-    
+
     // if-statement
     if (val%2==0){
-        printf("Even Number")
+        printf("Even Number\n");
+    } 
+    else if (val%3==0){
+        goto label;
     }
     // if-else statement
+    printf("Out of Goto\n");
+    return;
+    label: printf("Goto In\n");
+    switch (val+1){
+    case 4: printf("It is 3\n");
+    break;
+    case 10: printf("It is 9\n");
+    break;
+    default:
+    printf("It is nothing\n");
+    }
 
     // else-if ladder
 
