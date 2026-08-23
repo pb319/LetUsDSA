dbca63282921b238d25ee5ff4447cc78bb496545
pb319 <pranaybiswas319@gmail.com>
2026-08-23 20:45:59 +0530

    Byte Difference

--- C-Programming/04_pointer.c
+++ C-Programming/04_pointer.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 void main()
 {
-    int* pt1;
-    int val;
+    int  *pt1, *pt2;
+    int val1, val2;
     printf("Enter an Integer\n");
-    scanf("%d",&val);
-    printf("Your Input: %d\n",val);
-    return;
+    scanf("%d%d",&val1, &val2);
+    pt1 = &val1;
+    pt2 = &val2;
+    printf("Your Input: %d\n",val1);
+    printf("Address of Input: %d\n",pt1);
+    printf("Address of Input: %d\n",pt2);
+    printf("Difference: %d\n", pt2 - pt1);
+    printf("--- Integer Types ---\n");
+    printf("char      : %zu byte(s)\n", sizeof(char));
+    printf("short     : %zu byte(s)\n", sizeof(short));
+    printf("int       : %zu byte(s)\n", sizeof(int));
+    printf("long      : %zu byte(s)\n", sizeof(long));
+    printf("long long : %zu byte(s)\n", sizeof(long long));
+
+    printf("\n--- Floating-Point Types ---\n");
+    printf("float     : %zu byte(s)\n", sizeof(float));
+    printf("double    : %zu byte(s)\n", sizeof(double));
+    
+    printf("\n--- Pointer Types ---\n");
+    printf("int*      : %zu byte(s)\n", sizeof(int*));
+    printf("char*     : %zu byte(s)\n", sizeof(char*));
+
+    return 0;
 }
