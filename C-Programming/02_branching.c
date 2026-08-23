17fe92d0ca262ef62695cec5ba7c78b6de6103ff
pb319 <pranaybiswas319@gmail.com>
2026-08-23 19:36:48 +0530

    branching added

---
+++ C-Programming/02_branching.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+void main(){
+    int val;
+    printf("Enter an Integer:\n");
+    scanf("%d",&val);
+    
+    // if-statement
+    if (val%2==0){
+        printf("Even Number")
+    }
+    // if-else statement
+
+    // else-if ladder
+
+    // goto statement
+
+    // switch-case
+
+    return;
+}
