a79555fca1cc9a162b2393028a0d64feaf21dfec
pb319 <pranaybiswas319@gmail.com>
2026-08-25 07:52:25 +0530

    2D-Checkup

--- C-Programming/06_2Darray.c
+++ C-Programming/06_2Darray.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 void main(){
-    int arr[4][3]={{1,2,3},{3,4,5}};
+    int arr1[3][3]={1,2,3,3,4,5,5,6,7};
+    int arr2[][3]={1,2,3,3,4,5,5,6,7};
+    // int arr3[3][]={1,2,3,3,4,5,5,6,7};
     printf("The Array:\n");
-    for(int i=0;i<4;i++){
+    for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            printf("%d ",arr[i][j]);
+            printf("%d ",arr1[i][j]);
         }
         printf("\n");
     }
     printf("Thank You\n");
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("%d ",arr2[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Thank You\n");
+
+    //  for(int i=0;i<3;i++){
+    //     for(int j=0;j<3;j++){
+    //         printf("%d ",arr3[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // printf("Thank You\n");
     return;
 }
