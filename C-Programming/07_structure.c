b32afc1d72fc39f940368260faaeeba5c01c4e6f
pb319 <pranaybiswas319@gmail.com>
2026-09-01 12:31:34 +0000

    Structure Added

--- C-Programming/07_structure.c
+++ C-Programming/07_structure.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+struct student{
+    int rno;
+    int age;
+    float fees;
+};
 
 void main(){
-
+    struct student s1, s2;
+    printf("%d Bytes \n",sizeof(s1));
 
     return;
 }
