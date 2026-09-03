ad98e99851c2ae5c372565fe69209c75c9b7ab3f
pb319 <pranaybiswas319@gmail.com>
2026-09-03 20:22:47 +0530

    Stucture Changed

--- C-Programming/07_structure.c
+++ C-Programming/07_structure.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
-struct student{
-    int rno;
-    int age;
-    float fees;
-};
+
 
 void main(){
-    struct student s1, s2;
-    printf("%d Bytes \n",sizeof(s1));
+
 
     return;
 }
