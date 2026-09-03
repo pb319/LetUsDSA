72e1507903979dcd66f2135ea2a5f20ccf876a6c
pb319 <pranaybiswas319@gmail.com>
2026-09-03 20:34:12 +0530

    Stdent Data Retrieved

--- C-Programming/07_structure.c
+++ C-Programming/07_structure.c
@@ -1,3 +1,5 @@
+// Write a code to ask user enter details of two students. Once entered. print the roll numbers one by one.
+
 #include <stdio.h>
 
 struct student{
@@ -7,7 +9,17 @@ struct student{
 };
 
 void main(){
+    struct student s1;
     
+    printf("Enter Details:\nRoll Number:\n");
+    scanf("%d",&s1.rno);
+    printf("Enter Age:\n");
+    scanf("%d",&s1.age);
+    printf("Enter Fees:\n");
+    scanf("%f",&s1.fees);
+    
+    printf("Roll Number-%d Enrollment Complete\n",s1.rno);
+
 
     return;
 }
