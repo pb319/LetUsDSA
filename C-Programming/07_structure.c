36fcae6965ef0e20ca027a854f6f164480993513
pb319 <pranaybiswas319@gmail.com>
2026-09-05 17:21:32 +0530

    Arrow and Dot Operator

--- C-Programming/07_structure.c
+++ C-Programming/07_structure.c
@@ -9,7 +9,7 @@ struct student{
 };
 
 void main(){
-    struct student s1;
+    struct student s1, *s; // declaration of a pointer of datatype struct student
     
     printf("Enter Details:\nRoll Number:\n");
     scanf("%d",&s1.rno);
@@ -17,8 +17,13 @@ void main(){
     scanf("%d",&s1.age);
     printf("Enter Fees:\n");
     scanf("%f",&s1.fees);
-    
+    s= &s1; //assigning adderess of s1 structure
     printf("Enrollment of Roll Number-%d Complete\n",s1.rno);
+    printf("The Pointer stores %u\n",s);
+    printf("The Pointer points to %d\n",*s);
+    printf("The (*s+1) points to %u\n",(*s+1));
+    printf("Address of s1.age %u\n",&(s1.age));
+    printf("The s->age points to %u\n",s->age);
 
 
     return;
